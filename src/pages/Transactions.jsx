3ae8593bcb375b122c9ceb1020aa350d5c5b0b9f
khushi-Gfuture <khushi.gfuture@gmail.com>
2025-11-27 16:17:02 +0530

    Add files via upload

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,13 +1,42 @@
-import react from 'react';
+import React, { useState } from 'react';
+import './Transactions.css';
 
-const Transactions = () => {
-    return(
-        <div>
-            <h3>Transactions
-                <p>Transactions appear and shown here.</p>
-            </h3>
-        </div>
-    )
-}
+const Transactions = () =>{
+const [Transactions] = useState([
+    { id:1, Name:'Henry', Amount:'237', Date:'27-11-2025', status:'credited'},
+    { id:2, Name:'sam', Amount:'50', Date:'27-11-2025', status:'Debited'},
+    { id:3, Name:'Jenifer', Amount:'533', Date:'26-11-2025', status:'Debited'},
+    { id:4, Name:'Mariya', Amount:'700', Date:'27-11-2025', status: 'credited'},
+]);
+
+return(
+    <div className='Transactions-container'>
+        <h2>Transactions Details</h2>
+        <table className='Transactions-table'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>AMOUNT</th>
+                    <th>DATE</th>
+                    <th>STATUS</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Transactions.map((Transactions)=>(
+                    <tr key={Transactions.id}>
+                        <td>{Transactions.id}</td>
+                        <td>{Transactions.Name}</td>
+                        <td>{Transactions.Amount}</td>
+                        <td>{Transactions.Date}</td>
+                        <td>{Transactions.status}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+)
+};
 
 export default Transactions;
+
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,12 +1,42 @@
-import react from 'react';
+import React, { useState } from 'react';
+import './Users.css';
 
 const Users = () => {
-    return(
-        <div>
-            <h3>Users</h3>
-            <p>user details and their activities will shown here.</p>
-        </div>
-    )
-}
+  const [users] = useState([
+    { id: 1, name: 'John', email: 'john@gmail.com', phone: '111111111', role: 'Accountant' },
+    { id: 2, name: 'samm', email: 'samm@gmail.com', phone: '675645678', role: 'Manager' },
+    { id: 3, name: 'martin', email: 'martin@gmail.com', phone: '444444444', role: 'Developer'},
+    { id: 4, name: 'charles', email: 'charles@gmail.com', phone: '893789376', role: 'Manager' },
+    { id: 5, name: 'witson', email: 'witson@gmail.com', phone: '999900000', role: 'QA',  },
+  ]);
+
+  return (
+    <div className="users-container">
+      <h2>Users Details</h2>
+      <table className="users-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Users;
